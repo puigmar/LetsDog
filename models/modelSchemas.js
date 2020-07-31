a4381da82127f0dc2ce12918daa8eabb4323a17a
puigmar <ferran.puigmar@gmail.com>
2020-07-31 12:03:08 +0200

    models

--- models/modelSchemas.js
+++ models/modelSchemas.js
@@ -9,138 +9,174 @@ const usersSchema = new Schema (
             type: Boolean,
             default: false
         }
+    },
+    {
+        timestamps: true
     }
 )
 
-const clientSchema = new Schema ({
-    userId: {type: Schema.Types.ObjectId, ref: 'User'},
-    favourites: [{type: Schema.Types.ObjectId, ref: 'Carer'}],
-    dogsId: [{type: Schema.Types.ObjectId, ref: 'Dog'}],
-    cards: [{type: Schema.Types.ObjectId, ref: 'Card'}],
-    coords: {
-        type: {
-            type: String,
-            enum: ['Point'],
-            required: true
-          },
-          coordinates: {
-            type: [Number],
-            required: true
-          }
-    }
-})
-
-const dogSchema = new Schema ({
-    userId: {type: Schema.Types.ObjectId, ref: 'User'},
-    photo: {
-        type: String,
-        default: '',
-    },
-    sex: {
-        type: String,
-        enum: ['macho', 'hembra']
-    },
-    breed: String,
-    Size: {
-        type: String,
-        enum: ['pequeño', 'mediano', 'grande']
-    },
-    age: Number
-})
-
-const carerSchema = new Schema ({
-    userId: {type: Schema.Types.ObjectId, ref: 'User'},
-    profilePhoto: {
-        type: String,
-        default: '',
-    },
-    gallery: [String],
-    description: String,
-    items: {
-        ontime: {
-            name: 'Puntual',
-            score: Number
-        },
-        professional: {
-            name: 'Profesional',
-            score: Number
-        },
-        loving: {
-            name: 'Cariños@',
-            score: Number
-        },
-        atentive: {
-            name: 'Atent@',
-            score: Number
+const clientSchema = new Schema (
+    {
+        userId: {type: Schema.Types.ObjectId, ref: 'User'},
+        favourites: [{type: Schema.Types.ObjectId, ref: 'Carer'}],
+        dogsId: [{type: Schema.Types.ObjectId, ref: 'Dog'}],
+        cards: [{type: Schema.Types.ObjectId, ref: 'Card'}],
+        coords: {
+            type: {
+                type: String,
+                enum: ['Point'],
+                required: true
+            },
+            coordinates: {
+                type: [Number],
+                required: true
+            }
         }
     },
-    coords: {
-        type: {
-            type: String,
-            enum: ['Point'],
-            required: true
-          },
-          coordinates: {
-            type: [Number],
-            required: true
-          }
+    {
+        timestamps: true
     }
-})
+)
 
-const reviewsSchema = new Schema ({
-    userId: {type: Schema.Types.ObjectId, ref: 'User'},
-    carerId: {type: Schema.Types.ObjectId, ref: 'User'},
-    dogId: {type: Schema.Types.ObjectId, ref: 'Dog'},
-    average: Number,
-    items: {
-        ontime: {
-            name: 'Puntual',
-            score: Number
+const dogSchema = new Schema (
+    {
+        userId: {type: Schema.Types.ObjectId, ref: 'User'},
+        photo: {
+            type: String,
+            default: '',
         },
-        professional: {
-            name: 'Profesional',
-            score: Number
+        sex: {
+            type: String,
+            enum: ['macho', 'hembra']
         },
-        loving: {
-            name: 'Cariños@',
-            score: Number
+        breed: String,
+        Size: {
+            type: String,
+            enum: ['pequeño', 'mediano', 'grande']
         },
-        atentive: {
-            name: 'Atent@',
-            score: Number
+        age: Number
+    },
+    {
+        timestamps: true
+    }
+)
+
+const carerSchema = new Schema (
+    {
+        userId: {type: Schema.Types.ObjectId, ref: 'User'},
+        profilePhoto: {
+            type: String,
+            default: '',
+        },
+        gallery: [String],
+        description: String,
+        items: {
+            ontime: {
+                name: 'Puntual',
+                score: Number
+            },
+            professional: {
+                name: 'Profesional',
+                score: Number
+            },
+            loving: {
+                name: 'Cariños@',
+                score: Number
+            },
+            atentive: {
+                name: 'Atent@',
+                score: Number
+            }
+        },
+        coords: {
+            type: {
+                type: String,
+                enum: ['Point'],
+                required: true
+            },
+            coordinates: {
+                type: [Number],
+                required: true
+            }
         }
     },
-    description: String
-})
+    {
+        timestamps: true
+    }
+)
 
-const favouritesSchema = new Schema ({
-    userId: {type: Schema.Types.ObjectId, ref: 'User'},
-    carerId: [{type: Schema.Types.ObjectId, ref: 'User'}],
-})
+const reviewsSchema = new Schema (
+    {
+        userId: {type: Schema.Types.ObjectId, ref: 'User'},
+        carerId: {type: Schema.Types.ObjectId, ref: 'User'},
+        dogId: {type: Schema.Types.ObjectId, ref: 'Dog'},
+        average: Number,
+        items: {
+            ontime: {
+                name: 'Puntual',
+                score: Number
+            },
+            professional: {
+                name: 'Profesional',
+                score: Number
+            },
+            loving: {
+                name: 'Cariños@',
+                score: Number
+            },
+            atentive: {
+                name: 'Atent@',
+                score: Number
+            }
+        },
+        description: String
+    },
+    {
+        timestamps: true
+    }
+)
 
-const cardsSchema = new Schema ({
-    userId: {type: Schema.Types.ObjectId, ref: 'User'},
-    ownerName: String,
-    cardType: {
-        type: String,
-        enum: ['visa', 'master', 'american']
+const favouritesSchema = new Schema (
+    {
+        userId: {type: Schema.Types.ObjectId, ref: 'User'},
+        carerId: [{type: Schema.Types.ObjectId, ref: 'User'}],
     },
-    cardNumber: String,
-    expiration: {
-        type: Date
-    },
-    cvv: Number
-})
+    {
+        timestamps: true
+    }
+)
 
-const contractSchema = new Schema ({
-    userId: {type: Schema.Types.ObjectId, ref: 'User'},
-    carerId: [{type: Schema.Types.ObjectId, ref: 'User'}],
-    price: Number,
-    interval_price: Number,
-    meeting_point: String,
-    cardNumber: String,
-    expiration: {
-        type: Date
+const cardsSchema = new Schema (
+    {
+        userId: {type: Schema.Types.ObjectId, ref: 'User'},
+        ownerName: String,
+        cardType: {
+            type: String,
+            enum: ['visa', 'master', 'american']
+        },
+        cardNumber: String,
+        expiration: {
+            type: Date
+        },
+        cvv: Number
     },
-    cvv: Number
-})
+    {
+        timestamps: true
+    }
+)
+
+const contractSchema = new Schema (
+    {
+        userId: {type: Schema.Types.ObjectId, ref: 'User'},
+        carerId: [{type: Schema.Types.ObjectId, ref: 'User'}],
+        price: Number,
+        interval_price: Number,
+        meeting_point: String,
+        cardNumber: String,
+        expiration: Date,
+        cvv: Number
+    },
+    {
+        timestamps: true
+    }
+)
