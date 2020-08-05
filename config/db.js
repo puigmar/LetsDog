b3adae338ccc6cb6b8b64954b95165851455e1d3
puigmar <ferran.puigmar@gmail.com>
2020-08-05 12:01:35 +0200

    env heroku variables

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-
+require('dotenv').config()
 mongoose
-  .connect("mongodb://localhost/letsdog", {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
